5db6b56063e9c3e3236de615c1ba48e52079119a
Admin <Admin>
2016-11-09 13:46:43 +0100

    3D Vector class added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Vector
     {
         static void Main(string[] args)
         {
+            Vector3d vector1 = new Vector3d(12, 2, 7);
+            Vector3d vector2 = new Vector3d(-2, -2, 7);
+
+            Vector3d result = vector1.add(vector2);
+
+            Console.WriteLine("X = {0}", result.X);
+            Console.WriteLine("Y = {0}", result.Y);
+            Console.WriteLine("Z = {0}", result.Z);
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ Vector3d.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    class Vector3d
+    {
+        private int x, y, z;
+
+        public Vector3d(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+        public int Z { get { return z; } }
+
+        public double getLength()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        public Vector3d add(Vector3d addition)
+        {
+            int resultX = x + addition.x;
+            int resultY = y + addition.y;
+            int resultZ = z + addition.z;
+
+            return new Vector3d(resultX, resultY, resultZ);
+        }
+    }
+}
